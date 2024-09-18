323850aeceaadbb4a3cb333c258f5e838fcd0edc
ambi1303 <jhaambikesh555@gmail.com>
2024-09-18 17:45:44 +0530

    NEW FEATURES ADDED

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.elderlycare.app',
-  appName: 'ECSS',
-  webDir: 'dist/elderly_care/browser'
-};
-
-export default config;
